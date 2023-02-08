704c2f61d21492a91046c972500e3ce861d20ea6
Troy ker <dhker25@gmail.com>
2023-02-08 17:01:14 +0800

    second commit

--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -3,33 +3,31 @@ import './Nav.css';
 
 const Nav = () => {
   return (
-    <div>
-      <div className="logo">
-        <img src="logo/Logo.png" alt="Little Leomon Logo" width={280} height={78} />
-        <nav className='nav-bar'>
-        <ul>
-          <li>
-            <a href='Home'>Home</a>
-          </li>
-          <li>
-            <a href='About'>About</a>
-          </li>
-          <li>
-            <a href='Menu'>Menu</a>
-          </li>
-          <li>
-            <a href='Reservations'>Reservations</a>
-          </li>
-          <li>
-            <a href='Order Online'>Order Online</a>
-          </li>
-          <li>
-            <a href='Login'>Login</a>
-          </li>
-        </ul>
+    <header>
+        <img class='logo' src='logo/Logo.png' alt='Little Leomon Logo' />
+        <nav className='nav_link'>
+          <ul>
+            <li>
+              <a href='#Home'>Home</a>
+            </li>
+            <li>
+              <a href='#About'>About</a>
+            </li>
+            <li>
+              <a href='#Menu'>Menu</a>
+            </li>
+            <li>
+              <a href='#Reservations'>Reservations</a>
+            </li>
+            <li>
+              <a href='#Order Online'>Order Online</a>
+            </li>
+            <li>
+              <a href='#Login'>Login</a>
+            </li>
+          </ul>
         </nav>
-      </div>
-    </div>
+    </header>
   )
 }
 
