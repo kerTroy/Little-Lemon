07145762463c4194c2a185a95e29a63c686c0ea1
Troy ker <dhker25@gmail.com>
2023-02-08 17:58:51 +0800

    second commit

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -3,9 +3,20 @@ import './Main.css';
 
 const Main = () => {
   return (
-    <div>
-
+  <div className='grid-container'>
+    <div className='grid-item-1'>
+      <div className='grid-item-row-1'>Hero Section</div>
     </div>
+    <div className='grid-item-2'>
+      <div className='grid-item-row-2'>Highlight</div>
+    </div>
+    <div className='grid-item-3'>
+      <div className='grid-item-row-3'>Testimonials</div>
+    </div>
+    <div className='grid-item-4'>
+      <div className='grid-item-row-4'>About</div>
+    </div>
+  </div>
   )
 }
 
